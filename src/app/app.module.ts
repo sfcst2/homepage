9eaf3bc0593a2845295d673cedd4ee27e55f1c8c
sfcst2 <sfcst2@gmail.com>
2017-12-19 11:59:16 -0500

    Adding initial content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatButtonModule, MatMenuModule } from '@angular/material';
+import { ToptoolbarComponent } from './toptoolbar/toptoolbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,    
+    ToptoolbarComponent, HomepageComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
